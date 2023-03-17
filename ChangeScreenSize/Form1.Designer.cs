5b587a41cf103ea32b6e150fd617ec97846a3339
Ioana Popescu <mariap1313@yahoo.com>
2023-03-17 14:24:53 +0200

    Add project files.

---
+++ ChangeScreenSize/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ChangeScreenSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Scale Changer";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.exitApp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(614, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TOOL USED TO CHANGE SCREEN\'S SCALE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 54);
+            this.Controls.Add(this.label1);
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ChangeScreenSize/Form1.cs
@@ -0,0 +1,160 @@
+﻿using Dapplo.Windows.Common;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.Windows.Media;
+using Timer = System.Timers.Timer;
+
+namespace ChangeScreenSize
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int ChangeDisplaySettings([In] ref DEVMODE lpDevMode, int dwFlags);
+
+        Timer timer = new Timer();
+
+        public enum DMDO
+        {
+            DEFAULT = 0,
+            D90 = 1,
+            D180 = 2,
+            D270 = 3
+        }
+
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        struct DEVMODE
+        {
+            public const int DM_PELSWIDTH = 0x80000;
+            public const int DM_PELSHEIGHT = 0x100000;
+            private const int CCHDEVICENAME = 32;
+            private const int CCHFORMNAME = 32;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+
+            public int dmPositionX;
+            public int dmPositionY;
+            public DMDO dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+
+
+        private static void SetResolution(int w, int h)
+        {
+            long RetVal = 0;
+            DEVMODE dm = new DEVMODE();
+
+            dm.dmSize = (short)Marshal.SizeOf(typeof(DEVMODE));
+            dm.dmPelsWidth = w;
+            dm.dmPelsHeight = h;
+            dm.dmFields = DEVMODE.DM_PELSWIDTH | DEVMODE.DM_PELSHEIGHT;
+
+            RetVal = ChangeDisplaySettings(ref dm, 0);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            timer.Interval = 5000;
+            timer.Enabled = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var primaryMonitorScale = (100 * Screen.PrimaryScreen.Bounds.Width / SystemParameters.PrimaryScreenWidth);
+                var values1 = Registry.CurrentUser.GetSubKeyNames();
+                
+                var key = Registry.CurrentUser.OpenSubKey("Control Panel", true);
+                var values2 = key.GetSubKeyNames();
+                
+                key = key.OpenSubKey("Desktop", true);
+                var values3 = key.GetSubKeyNames();
+                
+                key = key.OpenSubKey("PerMonitorSettings", true);
+                
+                var monitors = key.GetSubKeyNames();
+                
+                RegistryKey auxKey = null;
+
+                auxKey = key.OpenSubKey(monitors[0], true);
+                if (Screen.AllScreens.Count() > 1)
+                {
+                    auxKey.SetValue("DpiValue", 0);
+                }
+                else auxKey.SetValue("DpiValue", -1);
+
+
+                SetResolution(1920, 1080);
+                SetResolution(2560, 1440);
+
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText("D:\\log.txt", "Exception thrown: " + ex.Message + ".\n");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer.Stop();
+            timer.Dispose();
+            timer.Close();
+        }
+
+        private void exitApp(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
